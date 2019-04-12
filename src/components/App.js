8c0a8e6a57f0e9ea6adbe34c901c73a340a4d35e
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-12 16:57:58 +0200

    action buttons to change state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { startGame, endGame } from '../actions/settings';
 
 class App extends Component {
+  startGame = () => {
+    this.props.dispatch(startGame());
+  }
+
+  endGame = () => {
+    this.props.dispatch(endGame());
+  }
+
   render() {
     return(
-      <div></div>
+      <div>
+        <h2>Odds or Evens</h2>
+        {
+          this.props.gameStarted ? (
+            <div>
+              <h3>The game has started</h3>
+              <hr />
+              <button type="button" className="btn btn-warning" onClick={this.endGame}>End the game</button>
+            </div>
+          ) : (
+            <div>
+              <h3>A new game awaits</h3>
+              <hr />
+              <button type="button" className="btn btn-info" onClick={this.startGame}>Start game</button>
+            </div>
+          )
+        }
+      </div>
     )
   }
 }
