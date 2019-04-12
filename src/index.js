ee04e67ae4996037d2b3557e5d7be9868cae5bb5
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-12 16:13:58 +0200

    checking out redux, reducer, subscribe, dispatch

--- src/index.js
+++ src/index.js
@@ -1,11 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import App from './components/App';
 import './index.css';
 
+const DEFAULT_SETTINGS = {
+  gameStarted: false,
+  instructionsExpanded: false
+}
+
+const SET_GAME_STARTED = 'SET_GAME_STARTED';
+const SET_INSTRUCTIONS_EXPANDED = 'SET_INSTRUCTIONS_EXPANDED';
+
+const rootReducer = (state = DEFAULT_SETTINGS, action) => {
+
+  switch(action.type) {
+    case SET_GAME_STARTED:
+      return {
+        gameStarted: action.gameStarted,
+        instructionsExpanded: state.instructionsExpanded
+      };
+    break;
+    case SET_INSTRUCTIONS_EXPANDED:
+      return {
+        gameStarted: state.gameStarted,
+        instructionsExpanded: action.instructionsExpanded
+      };
+    break;
+    default:
+      return state;
+    break;
+  }
+} 
+
+const store = createStore(rootReducer);
+
+store.subscribe(() => console.log('Store -->', store.getState()));
+
+const startGame = () => {
+  return { type: SET_GAME_STARTED, gameStarted: true };
+}
+
+const endGame = () => {
+  return { type: SET_GAME_STARTED, gameStarted: false };
+}
+
+const expandInstructions = () => {
+  return { type: SET_INSTRUCTIONS_EXPANDED, instructionsExpanded: true };
+}
+
+const foldInstructions = () => {
+  return { type: SET_INSTRUCTIONS_EXPANDED, instructionsExpanded: false };
+}
+
+store.dispatch(startGame());
+store.dispatch(expandInstructions());
+store.dispatch(endGame());
+store.dispatch(foldInstructions());
 
 ReactDOM.render(
   <App />,
    document.getElementById('root')
 );
-
