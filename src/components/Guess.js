0e18e747a9ca8bc21c0b9ecbdb390198e8492afa
Neil Kleynhans <ntkleynhans@gmail.com>
2019-04-18 13:47:00 +0200

    add record to local storage

--- src/components/Guess.js
+++ src/components/Guess.js
@@ -2,7 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { setGuessEven, setGuessOdd } from '../actions/gameState';
 
+const correctGuessesRecordKey = 'CORRECT_GUESSES_RECORD_KEY';
+
+const checkRecord = correctGuesses => {
+  const record = Number(localStorage.getItem(correctGuessesRecordKey) || 0);
+
+  if (correctGuesses > record) {
+    localStorage.setItem(correctGuessesRecordKey, correctGuesses);
+    return { record: correctGuesses, isNewRecord: true };
+  }
+  return { record: correctGuesses, isNewRecord: false };
+}
+
 const Guess = ({ guess, correctCount, setGuessEven, setGuessOdd }) => {
+
+  const { record, isNewRecord } = checkRecord(correctCount);
+
   return (
     <div>
       <h3>What is your card guess?</h3>
@@ -11,6 +26,7 @@ const Guess = ({ guess, correctCount, setGuessEven, setGuessOdd }) => {
       <button type="button" className={guess === 'odd' ? "btn btn-success" : "btn btn-info" } onClick={setGuessOdd}>Odd</button>
       <br />
       <p>Your correct guess count is: {correctCount} </p>
+      <p>{ isNewRecord ? 'New Record' : 'Record' }: {record} </p>
     </div>
   )
 }
--- src/reducers/gameState.js
+++ src/reducers/gameState.js
@@ -1,11 +1,11 @@
-import { GAME_STATE, SET_GAMES_STARTED, CARD } from '../actions/types';
+import { GAME_STATE, SET_GAME_STARTED, CARD } from '../actions/types';
 
 const DEFAULT_GAME_STATE = {
   guess: null,
   correctCount: 0
 }
 
-const EVEN = ['0', '2', '4', '6', '8'];
+const EVEN = ['0', '2', '4', '6', '8', '10'];
 const ODD = ['ACE', '3', '5', '7', '9'];
 
 const gameStateReducer = (state = DEFAULT_GAME_STATE, action) => {
@@ -14,7 +14,7 @@ const gameStateReducer = (state = DEFAULT_GAME_STATE, action) => {
       return { ...state, guess: action.guess };
     case GAME_STATE.GUESS_ODD:
       return { ...state, guess: action.guess };
-    case SET_GAMES_STARTED:
+    case SET_GAME_STARTED:
       return DEFAULT_GAME_STATE;
     case CARD.CARD_SUCCESS:
       let correctCount = state.correctCount;
